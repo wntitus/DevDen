f257bf49f7bab9ce9ee97c9420bcb645287ae511
wntitus <wesn.titus@gmail.com>
2018-08-30 14:01:17 -0400

    adding landing page and style for landing page, inside index.hbs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,69 @@
 var db = require("../models");
 
+// var path = require("path");
+
 module.exports = function(app) {
-  // Load index page
+  // Load Home page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+    res.render("index");
+  });
+
+  //Sign Up Page
+  app.get("/signup", function(req, res) {
+    res.render("?");
+  });
+
+  //Login Page
+  app.get("/login", function(req, res) {
+    res.render("?");
+  });
+
+  //Show Projects Page
+  app.get("/projects", function(req, res) {
+    db.Project.findAll().then(function(results) {
+      res.render("", {
+        project: results
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
+  //Show individual Project View
+  app.get("/project/:id", function(req, res) {
+    db.Project.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(results) {
+      res.render("?", {
+        project: results
       });
     });
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  //Profile Page
+  app.get("/user/:id", function(req, res) {
+    db.User.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(results) {
+      res.render("?", {
+        user: results
+      });
+    });
+  });
+
+  //Show Messages Page
+  app.get("/messages", function(req, res) {
+    db.Messages.findAll().then(function(results) {
+      res.render("?", {
+        messages: results
+      });
+    });
+
+    // Render 404 page for any unmatched routes
+    app.get("*", function(req, res) {
+      res.render("404");
+    });
   });
 };
