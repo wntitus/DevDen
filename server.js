66830360d43782956574fbb52fa9cfda5912170a
Timothy Bedsole <timbedsole@gmail.com>
2018-08-30 15:11:00 -0400

    Added api roiting for project, skill, and users.

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
+require("./routes/projectRoutes")(app);
+require("./routes/skillRoutes")(app);
+require("./routes/userRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
