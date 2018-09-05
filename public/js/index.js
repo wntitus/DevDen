185139662b28c78a43b7abfaa9454ae300b21c49
Timothy Bedsole <timbedsole@gmail.com>
2018-09-05 14:47:39 -0400

    Fixed weird issues that were stopping user page from loading

--- public/js/index.js
+++ public/js/index.js
@@ -26,28 +26,21 @@ $(document).ready(function() {
 
   var socket = io.connect();
   var room = "javascript";
-// 
-  // socket.on("join", function (room) {
-  //   console.log("join",room);
-
-  //   socket.join(room,function(){
-  //     console.log(sockets.rooms);
-  //   });
-    
-  // });
-  socket.on('connect', onConnect);
-	function onConnect() {
-		console.log('connected');
-		socket.emit('join', room);
-	}
-  socket.on("newMessage", function (message) {
+  socket.on("connect", onConnect);
+  function onConnect() {
+    console.log("connected");
+    socket.emit("join", room);
+  }
+  socket.on("newMessage", function(message) {
     var formatTime = moment(message.createdAt).format("h:mm");
     console.log("new message", message);
     var h4 = $("<h4></h4>");
     var p = $("<p></p>");
     h4.text(`${message.from}: ${formatTime}`);
     p.text(`${message.text}`);
-    $("#message-body").append(h4).append(p);
+    $("#message-body")
+      .append(h4)
+      .append(p);
   });
 
   socket.on("disconnect", function() {
@@ -56,12 +49,15 @@ $(document).ready(function() {
 
   $("#message-form").on("submit", function(event) {
     event.preventDefault();
-    socket.emit("createMessage", {
-      from: "user",
-      text: $("[name=message]").val()
-    }, function () {
-      $("[name=message]").val("")
-    });
+    socket.emit(
+      "createMessage",
+      {
+        from: "user",
+        text: $("[name=message]").val()
+      },
+      function() {
+        $("[name=message]").val("");
+      }
+    );
   });
-
-})
+});
